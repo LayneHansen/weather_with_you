53fef9319251444cd6e89aacdb3486a9b23e0752
LayneHansen <layne.d.hansen@gmail.com>
2020-08-29 17:28:34 -0600

    printed search city to list; made city clickable event, conditional statement to run function

--- script.js
+++ script.js
@@ -1,130 +1,126 @@
-
-
-
-// City Name appears once the search has been submitted
-
-var currentLat;
-var currentLon;
-
-// Push to local storage, retrieve from local storage to prior search fields
-
-
-// Functionality for text entered into search field
-// happens when page is refreshed
-
-
-// Functionality for search button
-
-
 // Data prints to data card with city name above
-
-var cityName = $("#city-name");
-console.log(cityName);
-
-// Forecasts for upcoming days appear in forecast windows
-
-var dayOne;
-var dayTwo;
-var dayThree;
-var dayFour;
-var dayFive;
-
-
-// FUNCTION TO SET AND GET TEXT
-
-// $("#search-button").on('click', function () {
-    
-//     var citySearch = $("#search-window");
-//     console.log(citySearch);
-//     localStorage.setItem("city search", citySearch);
-
-// })
-
-// $("#search-window").val(localStorage.getItem("city search"));
-
-function citySearchText (searchText) {
-
-    var textField = $(searchText);
-    return textField.val();
-
-}
-
-citySearchText ();
-console.log(citySearchText);
-
-
 
 var APIkey = "61a614007c0953840cad0a62bc2a2041";
 
-var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=minutely,hourly&appid=" + APIkey;
 
-// var queryURL = "https://api.openweathermap.org/data/2.5/onecall?
-// + "lat=" + currentLat + "&lon=" + currentLon + "&exclude=minutely,hourly" + "&appid=" + APIkey;
-
-// GOOGLE MAPS
-
-// application key: 
-
-// var settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://google-maps-geocoding.p.rapidapi.com/geocode/json?language=en&latlng=40.714224%252C-73.96145",
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "google-maps-geocoding.p.rapidapi.com",
-// 		"x-rapidapi-key": "a7bbd14756mshc19fbfa5df97acep1021fejsn94a760da9a7f"
-// 	}
-// }
-
-// $.ajax(settings).done(function (response) {
-// 	console.log(response);
-// });
-
-// var searchLat;
-// var searchLon;
+var cityName = $("#city-name");
+var citySearch;
+var searchLat;
+var searchLon;
+var searchList;
+var searchListItem;
 
 
-// AJAX 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
+// GLOBAL FUNCTION
+
+// $(document).ready(function () {
+
+$("#search-button").on('click', function () {
+    event.preventDefault();
+
+    citySearch = $("#search-window").val();
+    cityName.text(citySearch);
+
+
+if (citySearch !== "") {
+    searchCities(citySearch);
+
+} 
+
 })
 
-.then(function(response) {
-    
+var tempItem = localStorage.getItem("city search");
+console.log(tempItem);
+$("#search-1").text(tempItem);
+
+$("#search-1").on("click", function (event) {
+    var value = $("#search-1").text();
+    searchCities(value);
+
+})
+
+
+
+if (tempItem) {
+    searchCities(tempItem);
+
+} 
+
+function searchCities(citySearch) {
+    localStorage.setItem("city search", citySearch);
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIkey;
     console.log(queryURL);
-    console.log(response);
-    
-    var tempF = $("#temp");
-    tempF = (response.current.temp - 273.15) * 1.8 + 32;
-    $("#temp").text(" " + tempF.toFixed(2));
-    $("#humid").text(" " + response.current.humidity);
-    $("#wind").text(" " + response.current.wind_speed);
-    $("#uv").text(" " + response.current.uvi);
-    $("#day-1-temp").text(" " + response.daily[0].temp);
-    $("#day-1-humid").text(" " + response.daily[0].humidity);
-    $("#day-2-temp").text(" " + response.daily[1].temp);
-    $("#day-2-humid").text(" " + response.daily[1].humidity);
-    $("#day-3-temp").text(" " + response.daily[2].temp);
-    $("#day-3-humid").text(" " + response.daily[2].humidity);
-    $("#day-4-temp").text(" " + response.daily[3].temp);
-    $("#day-4-humid").text(" " + response.daily[3].humidity);
-    $("#day-5-temp").text(" " + response.daily[4].temp);
-    $("#day-5-humid").text(" " + response.daily[4].humidity);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        var cityName = $("#city-name");
+        cityName = $("#search-window").val();
+        searchLat = response.coord.lat;
+        searchLon = response.coord.lon;
+
+        weatherData();
+
+    })
+
+}
 
 
-    // pull data
 
-    // set to local storage
+// FUNCTION FOR ONE CALL API
 
-    // populate everything in list, get from local storage
-    
-    // separate api call for uv index
 
-    // separate api call for 5-day
-    
-})
+function weatherData() {
+
+    var queryTWO = "https://api.openweathermap.org/data/2.5/onecall?&units=imperial&lat=" + searchLat + "&lon=" + searchLon + "&exclude=minutely,hourly&appid=" + APIkey;
+
+    $.ajax({
+        url: queryTWO,
+        method: "GET"
+    })
+
+
+        .then(function (response) {
+
+            console.log(queryTWO);
+            console.log(response);
+            // var uvi = $("#uv").val(response.current.uvi);
+            // console.log(uvi);
+
+            // if uvi =< 3 {
+
+            // }
+
+            $("#temp").text(" " + response.current.temp);
+            $("#humid").text(" " + response.current.humidity);
+            $("#wind").text(" " + response.current.wind_speed);
+            $("#uv").text(" " + response.current.uvi);
+            $("#icon").attr("src", "http://openweathermap.org/img/wn/" + response.current.weather[0].icon + "@2x.png");
+            //$("#date-1").text(moment().add(1, "days");
+            $("#day-1-temp").text(" " + response.daily[1].temp.max);
+            $("#day-1-humid").text(" " + response.daily[1].humidity);
+            $("#icon-1").attr("src", "http://openweathermap.org/img/wn/" + response.daily[1].weather[0].icon + "@2x.png");
+            $("#day-2-temp").text(" " + response.daily[2].temp.max);
+            $("#day-2-humid").text(" " + response.daily[2].humidity);
+            $("#icon-2").attr("src", "http://openweathermap.org/img/wn/" + response.daily[2].weather[0].icon + "@2x.png");
+            $("#day-3-temp").text(" " + response.daily[3].temp.max);
+            $("#day-3-humid").text(" " + response.daily[3].humidity);
+            $("#icon-3").attr("src", "http://openweathermap.org/img/wn/" + response.daily[3].weather[0].icon + "@2x.png");
+            $("#day-4-temp").text(" " + response.daily[4].temp.max);
+            $("#day-4-humid").text(" " + response.daily[4].humidity);
+            $("#icon-4").attr("src", "http://openweathermap.org/img/wn/" + response.daily[4].weather[0].icon + "@2x.png");
+            $("#day-5-temp").text(" " + response.daily[5].temp.max);
+            $("#day-5-humid").text(" " + response.daily[5].humidity);
+            $("#icon-5").attr("src", "http://openweathermap.org/img/wn/" + response.daily[5].weather[0].icon + "@2x.png");
+
+        })
+
+}
+
+
 
 
 // Display date and time on navbar with moment.js
